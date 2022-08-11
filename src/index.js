1b758657a40b65ead26793dd2b62ef34cb36b5c9
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-11 15:48:47 -0500

    commit tree 5c0f05786029d433ab17e12702bb714a66f8921e

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
+const movieURL = 'http://localhost:3000/movies'
+
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID')
+        const id = input.value;
+
+        fetch(`${movieURL}/${id}`)
+        .then(res => res.json())
+        .then(displayMovie)
+
+    });
+
+    function displayMovie(movie) {
+        // return console.log(movie);
+        const movieForm = document.querySelector('#movieDetails')
+        const h4 = movieForm.getElementsByTagName('h4')[0]
+        const p = movieForm.getElementsByTagName('p')[0]
+
+        h4.textContent = movie.title
+        p.textContent = movie.summary
+        // console.log(h4, p);
+    }
+
   
 }
 
